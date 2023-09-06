b1889add2cd5553fcbe569a6ba2dd87f4a5e0fa4
Alex <alexnowrang@gmail.com>
2023-09-05 20:56:04 -0500

    Add data set for plot, setting, characters and genre

---
+++ mixed-messages.js
@@ -0,0 +1,9 @@
+//story idea generator
+
+//object of data to choose from
+const  storyData = {
+  genre: ['historical fiction', 'romance', 'comedy', 'horror', 'narative', 'mystery', 'thriller', 'science fiction', 'western'],
+  plot: ['"rags to riches"', '"voyage and return"', '"tragedy"', '"rebirth"', '"coming of age"', '"quest"'],
+  characters: ['Nelson Mandela', 'Martha Stewert', 'Bart Simpson', 'Abe Lincoln', 'Bob Marley', 'Huey Lewis', 'Issa Rae', 'Ted Nugent', 'Janis Joplin', 'Tom Brady', 'Biggie Smalls'],
+  setting: ['in the mountains', 'in a frozen wasteland', 'in Velaris', 'in a meadow', 'in a cove', 'in a post apocalyptic world', 'in a hounted house']  
+}
